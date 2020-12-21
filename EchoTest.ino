6208230bcab68a6d5b896950966e1c1d77a3ceec
AndreasE <63844978+AndreasExner@users.noreply.github.com>
2020-12-21 12:09:01 +0100

    Update EchoTest.ino

--- EchoTest.ino
+++ EchoTest.ino
@@ -1,11 +1,11 @@
 /*
 
-  RS485/UART echo
+  Serial/UART echo
 
   Version: 1.0
   Date: 2020-12-15
 
-  This sketch is useful for debugging and testing RS485/UART wirings. 
+  This sketch is useful for debugging and testing Serial/UART wirings. 
   Often the remote device is mounted somewhere on a rooftop or another 
   inconvenient place. That's where this tool comes in and offers you a 
   simulation for your lab.
@@ -13,9 +13,9 @@
   It was intentionally made for the wind direction sensor:
   https://github.com/AndreasExner/iobroker-IoT-WindSensor
 
-  But it can be used for different purposes as well:
+  But it can be used for different purposes as well
 
-  
+
   
 
   MIT License
@@ -41,17 +41,15 @@
   SOFTWARE.
 */
 
-#include <SoftwareSerial.h>
+#define RTS D5 // RS485 bus master
+#define LED D3
 
-#define RX D6
-#define TX D7
-#define RTS D5
-#define BAUD_RATE 9600
-#define LED D4
+#define SERIAL_BAUD_RATE 9600 //baud rate for RS485
+#define DEBUG_BAUD_RATE 115200 //baud rate for FT232 / serial1
 
 int counter=0;
+bool debug = true;
 
-SoftwareSerial RS485;
 
 byte windSensorFrame[] = {0x01, 0x03, 0x04, 0x00, 0x5A, 0x00, 0x00, 0x00, 0x00}; // default answer frame for the wind sensor DO NOT CHANGE
 byte customFrame[] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // your custom answer frame (CRC will be calculated automatically)
@@ -59,19 +57,27 @@ byte customFrame[] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // 
 bool useWindSensorFrame = true; // echos a random wind direction if true, a custom frame or a copy of the incoming frame if false
 bool useCustomFrame = false; // echos the custom frame if true, a copy of the incoming frame if false 
 
+int expectedFrameSize = 8; // expected RX frame size
+
 
 //######################################### setup ##############################################################
 
 void setup(void) {
 
-  Serial.begin(115200);  
+  Serial1.begin(DEBUG_BAUD_RATE);
+  delay(1000);
+
+  Serial.begin(SERIAL_BAUD_RATE);
   delay(1000);
   
+  
   pinMode(LED, OUTPUT);
-
   pinMode(RTS, OUTPUT);
   digitalWrite(RTS, LOW);
-  RS485.begin(BAUD_RATE, SWSERIAL_8N1, RX, TX, false, 128, 128);
+  digitalWrite(LED, LOW);
+
+  if (debug) {Serial1.println("Started....");}
+  
 
 }
 
@@ -80,134 +86,183 @@ void setup(void) {
   
 void loop(void) {
 
-// read from buffer
+// ----------------------- read from buffer
 
   digitalWrite(RTS, LOW);
-  int readBufferSize = RS485.available();
+  int readBufferSize;
+
+  int x = 10;
+  while (x > 0) {  // enables loop every 50 ms
   
-  if(readBufferSize > 0){
-    
-    byte readBuffer[readBufferSize];
-    Serial.println("RX Buffer Size: " + String(readBufferSize));
-    RS485.readBytes(readBuffer, readBufferSize);
-  
-    Serial.print("RX Buffer : ");
-      for ( byte i = 0; i < readBufferSize; i++ ) {
-        Serial.print(hex_to_string(readBuffer[i]));
-        Serial.print(" ");
-      }
-      Serial.print("\n");
+      readBufferSize = Serial.available();
+      if (readBufferSize >= expectedFrameSize) {break;}
+      delay(5);
+      x--;
+  }
 
-    delay(10);
-
-// CRC Check
-
-    uint16_t CRC;
-    uint8_t CRC_MSB;
-    uint8_t CRC_LSB;
-    String CRC_Output;
-    
-    CRC = Calc_CRC(readBuffer, readBufferSize);
-    CRC_MSB = CRC >> 8;
-    CRC_LSB = CRC;
-    CRC_Output = hex_to_string(CRC_LSB) + " " + hex_to_string(CRC_MSB);
-
-    if (readBuffer[readBufferSize -2] == CRC_LSB && readBuffer[readBufferSize -1] == CRC_MSB) {
-
-// send echo
-
-      if (useWindSensorFrame) { // send wind direction frame
-
-        int echoBufferSize = sizeof(windSensorFrame);
+// ----------------------- proceed whe buffer is big enogh to expect a full frame
  
-        uint16_t windDir = getRandomWindDir(); // add random wind direction
+  if(readBufferSize >= expectedFrameSize) {
+  
+    digitalWrite(LED, LOW);
+    byte readBuffer[readBufferSize];
+    Serial.readBytes(readBuffer, readBufferSize);
+   
+    if (debug) {
+      Serial1.println("RX Buffer Size: " + String(readBufferSize));
+      Serial1.print("RX Buffer : ");
+        for ( byte i = 0; i < readBufferSize; i++ ) {
+          Serial1.print(hex_to_string(readBuffer[i]));
+          Serial1.print(" ");
+        }
+        Serial1.println("end");
+    }
+
+// ----------------------- trim leading zeros (errors) from frame
+
+    while (readBuffer[0] == 0) {
+      for (int i = 1; i < readBufferSize; i++) {
+        readBuffer[i - 1] = readBuffer[i];
+      }
+      readBufferSize--;
+    }
+
+
+    if (debug) {
+      Serial1.println("Trimmed frame size: " + String(readBufferSize));
+      Serial1.print("Frame : ");
+        for ( byte i = 0; i < readBufferSize; i++ ) {
+          Serial1.print(hex_to_string(readBuffer[i]));
+          Serial1.print(" ");
+        }
+        Serial1.println("end");
+    }
+
+// ----------------------- proceed whe buffer is still big enogh to expect a full frame
+
+    if (readBufferSize >= expectedFrameSize) {
+      
+// ----------------------- CRC Check
+  
+      uint16_t CRC = Calc_CRC(readBuffer, expectedFrameSize);
+      uint8_t CRC_MSB = CRC >> 8;
+      uint8_t CRC_LSB = CRC;
+      String CRC_Output = hex_to_string(CRC_LSB) + " " + hex_to_string(CRC_MSB);
+      bool CRCCheck;
+  
+      if (readBuffer[expectedFrameSize -2] == CRC_LSB && readBuffer[expectedFrameSize -1] == CRC_MSB) {
+        if(debug) {Serial1.println("CRC OK");}
+        CRCCheck = true;
+      }
+      else {
+        if(debug) {Serial1.println("CRC ERROR");}
+        CRCCheck = false;
+      }
+  
+// ----------------------- send answer: WindSensorFrame with random direction
+       
+      double ByteTime = (10000000 / SERIAL_BAUD_RATE); // RTS HIGH time for one byte in µs
+      
+      if (CRCCheck && useWindSensorFrame && !useCustomFrame) {
+    
+        int echoBufferSize = sizeof(windSensorFrame);
+        
+        uint16_t windDir = getRandomWindDir(); // add random wind direction from counter
         uint8_t windDir_MSB;
         uint8_t windDir_LSB;
         windDir_MSB = windDir >> 8;
         windDir_LSB = windDir;
         windSensorFrame[4] = windDir_LSB;
         windSensorFrame[3] = windDir_MSB;
-
-        CRC = Calc_CRC(windSensorFrame, echoBufferSize);
+    
+        CRC = Calc_CRC(windSensorFrame, echoBufferSize);  // calc CRC for frame
         CRC_MSB = CRC >> 8;
         CRC_LSB = CRC;
         windSensorFrame[echoBufferSize -2] = CRC_LSB;
         windSensorFrame[echoBufferSize -1] = CRC_MSB;
-  
-        Serial.println("WindDir " + String(counter));
-        Serial.println("TX Buffer size " + String(echoBufferSize) + " bytes");
-        
-        Serial.print("TX Buffer : ");
-        for ( byte i = 0; i < echoBufferSize; i++ ) {
-          Serial.print(hex_to_string(windSensorFrame[i]));
-          Serial.print(" ");
+    
+        if (debug) {
+          Serial1.println("TX Buffer size " + String(echoBufferSize) + " bytes");
+          
+          Serial1.print("TX Buffer : ");
+          for ( byte i = 0; i < echoBufferSize; i++ ) {
+            Serial1.print(hex_to_string(windSensorFrame[i]));
+            Serial1.print(" ");
+          }
+          Serial1.println("end");
         }
-        Serial.print("\n");
-  
+    
+        delay(1);
         digitalWrite(RTS, HIGH);
-        RS485.write(windSensorFrame, echoBufferSize);
+        Serial.write(windSensorFrame, echoBufferSize);
+        delayMicroseconds(echoBufferSize * ByteTime);
         digitalWrite(RTS, LOW); 
       }
-      else if (!useWindSensorFrame && useCustomFrame) {  // send custom frame
-        
+    
+// ----------------------- send answer: custom frame
+         
+      if (CRCCheck && !useWindSensorFrame && useCustomFrame) {
+                  
         int echoBufferSize = sizeof(customFrame);
       
-        CRC = Calc_CRC(customFrame, echoBufferSize);
+        CRC = Calc_CRC(customFrame, echoBufferSize); // calc CRC for frame
         CRC_MSB = CRC >> 8;
         CRC_LSB = CRC;
         customFrame[echoBufferSize -2] = CRC_LSB;
         customFrame[echoBufferSize -1] = CRC_MSB;
-  
-        Serial.println("TX Buffer size " + String(echoBufferSize) + " bytes");
-        
-        Serial.print("TX Buffer : ");
-        for ( byte i = 0; i < echoBufferSize; i++ ) {
-          Serial.print(hex_to_string(customFrame[i]));
-          Serial.print(" ");
+    
+        if (debug) {
+          Serial1.println("TX Buffer size " + String(echoBufferSize) + " bytes");
+          Serial1.print("TX Buffer : ");
+          for ( byte i = 0; i < echoBufferSize; i++ ) {
+            Serial1.print(hex_to_string(customFrame[i]));
+            Serial1.print(" ");
+          }
+          Serial1.println("end");
         }
-        Serial.print("\n");
-  
+        
+        delay(1);
         digitalWrite(RTS, HIGH);
-        RS485.write(customFrame, echoBufferSize);
+        Serial.write(customFrame, echoBufferSize);
+        delayMicroseconds(echoBufferSize * ByteTime);
         digitalWrite(RTS, LOW); 
       }
-      else {  // send read buffer
-
-        Serial.println("TX Buffer size " + String(readBufferSize) + " bytes");
-        
-        Serial.print("TX Buffer : ");
-        for ( byte i = 0; i < readBufferSize; i++ ) {
-          Serial.print(hex_to_string(readBuffer[i]));
-          Serial.print(" ");
+    
+// ----------------------- send answer: echo rx frame
+         
+      if (CRCCheck && !useWindSensorFrame && !useCustomFrame) {
+    
+        if (debug) {
+          Serial1.println("TX Buffer size " + String(expectedFrameSize) + " bytes");
+          
+          Serial1.print("TX Buffer : ");
+          for ( byte i = 0; i < expectedFrameSize; i++ ) {
+            Serial1.print(hex_to_string(readBuffer[i]));
+            Serial1.print(" ");
+          }
+          Serial1.println("end");
         }
-        Serial.print("\n");
-  
+        
+        delay(1);
         digitalWrite(RTS, HIGH);
-        RS485.write(readBuffer, readBufferSize);
+        Serial.write(readBuffer, expectedFrameSize);
+        delayMicroseconds(expectedFrameSize * ByteTime);
         digitalWrite(RTS, LOW); 
       }
-
+      Serial1.println("---------------");
+  
+      delay(x * 100);  // flush buffer
+      byte flushBuffer[readBufferSize];
+      Serial.readBytes(flushBuffer, readBufferSize); 
+      
     }
     else {
-      Serial.println("RX CRC ERROR");
-      RS485.flush();
+      if(debug) {Serial1.println("Trimmed frame to short");}
     }
-    
-    Serial.print("\n");
-    
   }
-  else {
-    //Serial.println("No data");
-  }
-  
-  if (counter % 2) {
-    digitalWrite(LED, LOW);         
-  }
-  else {
-    digitalWrite(LED, HIGH);
-  }
-  
-  delay(50);
+
+  digitalWrite(LED, HIGH);
+ 
   counter++;
   if (counter > 15) {counter = 0;}
 }
